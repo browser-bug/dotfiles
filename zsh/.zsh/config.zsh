94f3e45fff65594dad5ffaae8f5d63fb5e3ebc1f
Bernardo Menicagli <bernardo.menicagli@gmail.com>
2020-11-03 22:24:38 +0100

    direnv hook support

--- zsh/.zsh/config.zsh
+++ zsh/.zsh/config.zsh
@@ -17,3 +17,14 @@ setopt APPEND_HISTORY   # adds history
 # Key bindings
 bindkey '^H' backward-kill-word
 bindkey '^[[3^' kill-word
+
+# direnv
+eval "$(direnv hook zsh)"
+
+setopt PROMPT_SUBST
+show_virtual_env() {
+  if [[ -n "$VIRTUAL_ENV" && -n "$DIRENV_DIR" ]]; then
+    echo "($(basename $VIRTUAL_ENV)) "
+  fi
+}
+PS1='$(show_virtual_env)'$PS1
