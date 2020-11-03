7c8f2c1a4dcb7291f003cd4d35a9633563d0219a
Bernardo Menicagli <bernardo.menicagli@gmail.com>
2020-11-03 22:25:13 +0100

    Added kitty unicode input kitten alias

--- zsh/.zsh/aliases.zsh
+++ zsh/.zsh/aliases.zsh
@@ -1,5 +1,6 @@
 alias open='xdg-open'
 alias fd='fdfind'
+alias icat="kitty +kitten icat --align left"
 
 # https://github.com/emericg/OpenSubtitlesDownload
 alias opensubtitles='python3 ~/.local/share/nautilus/scripts/OpenSubtitlesDownload.py --cli'
