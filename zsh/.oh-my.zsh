f4622cfb0201f6f8ca3f6198b83002fb3883d8e7
Bernardo Menicagli <bernardo.menicagli@gmail.com>
2021-02-16 12:12:38 +0100

    Added spaceship theme to zsh.

--- zsh/.oh-my.zsh
+++ zsh/.oh-my.zsh
@@ -1,11 +1,13 @@
 # Path to your oh-my-zsh installation.
 export ZSH=$HOME/.oh-my-zsh
+# For some reasons the spaceship root dir is not inferred correctly by the setup script
+export SPACESHIP_ROOT=$ZSH/custom/themes/spaceship-prompt
 
 # Set name of the theme to load --- if set to "random", it will
 # load a random theme each time oh-my-zsh is loaded, in which c
 # to know which specific one was loaded, run: echo $RANDOM_THEM
 # See https://github.com/ohmyzsh/ohmyzsh/wiki/Themes
-ZSH_THEME="robbyrussell"
+ZSH_THEME="spaceship"
 
 # Which plugins would you like to load?
 # Standard plugins can be found in $ZSH/plugins/
@@ -16,3 +18,16 @@ plugins=(zsh-autosuggestions git python pip)
 
 source $ZSH/oh-my-zsh.sh
 
+if [ "$ZSH_THEME" = "spaceship" ]; then
+  source $ZSH/custom/spaceship_custom_settings.zsh
+else
+  # Showing hostname in prompt
+  PROMPT="%{$fg[white]%}%n@%{$fg[white]%}%m%{$reset_color%} ${PROMPT}"
+  setopt PROMPT_SUBST
+  show_virtual_env() {
+    if [[ -n "$VIRTUAL_ENV" && -n "$DIRENV_DIR" ]]; then
+      echo "($(basename $VIRTUAL_ENV)) "
+    fi
+  }
+  PS1='$(show_virtual_env)'$PS1
+fi
--- zsh/.zsh/config.zsh
+++ zsh/.zsh/config.zsh
@@ -3,9 +3,6 @@ export UPDATE_ZSH_DAYS=7
 
 COMPLETION_WAITING_DOTS="true"
 
-# Showing hostname in prompt
-PROMPT="%{$fg[white]%}%n@%{$fg[white]%}%m%{$reset_color%} ${PROMPT}"
-
 # History
 HISTFILE=~/.zsh_history
 HISTSIZE=100000
@@ -20,11 +17,3 @@ bindkey '^[[3^' kill-word
 
 # direnv
 eval "$(direnv hook zsh)"
-
-setopt PROMPT_SUBST
-show_virtual_env() {
-  if [[ -n "$VIRTUAL_ENV" && -n "$DIRENV_DIR" ]]; then
-    echo "($(basename $VIRTUAL_ENV)) "
-  fi
-}
-PS1='$(show_virtual_env)'$PS1
